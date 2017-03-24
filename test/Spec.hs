c73c15c9989176060c101900c7810978b5098695
Wai Lee Chin Feman <w.l.chin.feman@gmail.com>
2017-03-24 15:55:59 +0000

    lol renaming

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,77 +8,77 @@ test_make_onehigh =
                "make one-high with 10 of clubs"
                (HTOneHigh, [10, 5, 3, 2, 1])
                hand
-  where hand = make_hand ((Card Hearts 1), (Card Hearts 5), (Card Clubs 2), (Card Clubs 3), (Card Clubs 10))
+  where hand = makeHand ((Card Hearts 1), (Card Hearts 5), (Card Clubs 2), (Card Clubs 3), (Card Clubs 10))
 
 test_make_pair =
   TestCase $ assertEqual
                "make pair of ones"
                (HTPair, [1, 10, 8, 5])
                hand
-  where hand = make_hand ((Card Hearts 1), (Card Clubs 1), (Card Spades 5), (Card Clubs 8), (Card Clubs 10))
+  where hand = makeHand ((Card Hearts 1), (Card Clubs 1), (Card Spades 5), (Card Clubs 8), (Card Clubs 10))
 
 test_make_triple =
   TestCase $ assertEqual
                "make three of a kind with ones"
                (HTTriple, [1,10,8])
                hand
-  where hand = make_hand ((Card Hearts 1), (Card Clubs 1), (Card Spades 1), (Card Clubs 8), (Card Clubs 10))
+  where hand = makeHand ((Card Hearts 1), (Card Clubs 1), (Card Spades 1), (Card Clubs 8), (Card Clubs 10))
 
 test_make_twopair =
   TestCase $ assertEqual
                "make twopair"
                (HTTwoPair, [2, 1, 10])
                hand
-  where hand = make_hand ((Card Hearts 1), (Card Clubs 1), (Card Spades 2), (Card Clubs 2), (Card Clubs 10))
+  where hand = makeHand ((Card Hearts 1), (Card Clubs 1), (Card Spades 2), (Card Clubs 2), (Card Clubs 10))
 
 test_make_straight =
   TestCase $ assertEqual
                "make straight"
                (HTStraight, [5])
                hand
-  where hand = make_hand ((Card Hearts 1), (Card Clubs 2), (Card Spades 3), (Card Clubs 4), (Card Diamonds 5))
+  where hand = makeHand ((Card Hearts 1), (Card Clubs 2), (Card Spades 3), (Card Clubs 4), (Card Diamonds 5))
 
 test_make_flush =
   TestCase $ assertEqual
                "make flush"
                (HTFlush, [7,4,3,2,1])
                hand
-  where hand = make_hand ((Card Hearts 1), (Card Hearts 2), (Card Hearts 3), (Card Hearts 4), (Card Hearts 7))
+  where hand = makeHand ((Card Hearts 1), (Card Hearts 2), (Card Hearts 3), (Card Hearts 4), (Card Hearts 7))
 
 test_make_straight_flush =
   TestCase $ assertEqual
                "make straight flush"
                (HTStraightFlush, [6])
                hand
-  where hand = make_hand ((Card Hearts 2), (Card Hearts 3), (Card Hearts 4), (Card Hearts 5), (Card Hearts 6))
+  where hand = makeHand ((Card Hearts 2), (Card Hearts 3), (Card Hearts 4), (Card Hearts 5), (Card Hearts 6))
 
 test_make_full_house =
   TestCase $ assertEqual
                "make full house"
                (HTFullHouse, [3,1])
                hand
-  where hand = make_hand ((Card Hearts 1), (Card Clubs 1), (Card Hearts 3), (Card Spades 3), (Card Diamonds 3))
+  where hand = makeHand ((Card Hearts 1), (Card Clubs 1), (Card Hearts 3), (Card Spades 3), (Card Diamonds 3))
 
 test_full_hous_fight =
   TestCase $ assertBool
                "make full house"
                (badFullHouse < goodFullHouse)
-  where badFullHouse = make_hand ((Card Hearts 1), (Card Clubs 1), (Card Hearts 3), (Card Spades 3), (Card Diamonds 3))
-        goodFullHouse = make_hand ((Card Hearts 1), (Card Clubs 1), (Card Hearts 8), (Card Spades 8), (Card Diamonds 8))
+  where badFullHouse = makeHand ((Card Hearts 1), (Card Clubs 1), (Card Hearts 3), (Card Spades 3), (Card Diamonds 3))
+        goodFullHouse = makeHand ((Card Hearts 1), (Card Clubs 1), (Card Hearts 8), (Card Spades 8), (Card Diamonds 8))
 
 test_three_of_a_kind_fight_tied_kicker =
   TestCase $ assertBool
                "three of a kind with better second-kicker wins"
                (badTriple < goodTriple)
-  where badTriple = make_hand ((Card Hearts 1), (Card Clubs 1), (Card Spades 1), (Card Diamonds 10), (Card Diamonds 3))
-        goodTriple = make_hand ((Card Diamonds 1), (Card Clubs 1), (Card Hearts 1), (Card Spades 10), (Card Diamonds 8))
+  where badTriple = makeHand ((Card Hearts 1), (Card Clubs 1), (Card Spades 1), (Card Diamonds 10), (Card Diamonds 3))
+        goodTriple = makeHand ((Card Diamonds 1), (Card Clubs 1), (Card Hearts 1), (Card Spades 10), (Card Diamonds 8))
 
 test_fullhouse_beats_pair =
   TestCase $ assertBool
                "full house beats pair"
                (fullHouse > pair)
-  where fullHouse = make_hand ((Card Hearts 1), (Card Clubs 1), (Card Spades 1), (Card Diamonds 2), (Card Diamonds 2))
-        pair = make_hand ((Card Diamonds 1), (Card Clubs 1), (Card Hearts 3), (Card Spades 5), (Card Diamonds 9))
+  where fullHouse = makeHand ((Card Hearts 1), (Card Clubs 1), (Card Spades 1), (Card Diamonds 2), (Card Diamonds 2))
+        pair = makeHand ((Card Diamonds 1), (Card Clubs 1), (Card Hearts 3), (Card Spades 5), (Card Diamonds 9))
 
 test_parses_and_compares_hands =
   TestCase $ assertBool
